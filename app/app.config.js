c2253061eabc7cc71a2cecccded79b7dd645c45a
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2020-12-21 10:44:54 +0100

    phone-cat app created

---
+++ app/app.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('phonecatApp').config(['$routeProvider',
+  function config($routeProvider) {
+    $routeProvider.when('/phones', {
+      template: '<phone-list></phone-list>'
+    }).when('/phones/:phoneId', {
+      template: '<phone-detail></phone-detail>'
+    }).otherwise('/phones');
+  }
+]);
